5ed34927fb6266a86ecaa0b73b31889e80e0063d
Vannick Nonongo <vannicknonongo@gmail.com>
2026-05-03 11:17:41 +0100

    fix: deploying

--- src/services/agent.py
+++ src/services/agent.py
@@ -14,7 +14,7 @@ from datetime import date
 from typing import Optional
 
 from groq import BadRequestError
-from langchain.agents import AgentExecutor
+from langchain.agents.agent import AgentExecutor
 from langchain.agents.format_scratchpad.tools import format_to_tool_messages
 from langchain.agents.output_parsers.tools import ToolsAgentOutputParser
 from langchain_core.messages import AIMessage, AIMessageChunk, BaseMessage, HumanMessage
